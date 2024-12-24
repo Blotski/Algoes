2d8572a0991ce5417e2d21513dcc91745a2e868e
Vitaly <8lotski@gmail.com>
2024-12-24 08:04:16 -0500

    704

--- 206_ReverseLL.ts
+++ 206_ReverseLL.ts
@@ -1,23 +1,23 @@
 
 class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
 }
 
 
 function reverseList(head: ListNode | null): ListNode | null {
-    let prev: ListNode | null = null;
+  let prev: ListNode | null = null;
 
-    while (head) {
-        const next = head.next;
-        head.next = prev;
-        prev = head;
-        head = next;
-    }
+  while (head) {
+      const next: ListNode | null = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+  }
 
-    return prev;
+  return prev;
 };
